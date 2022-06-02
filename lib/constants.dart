88eb3f3959f93f397880de7025c18d20861ca95d
Alexander Razzhivin <admin@docode.ru>
2022-06-02 16:47:16 +0300

    fix: path to project.pbxproj

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,4 @@
 const String androidManifestFile = 'android/app/src/main/AndroidManifest.xml';
 const String iOSInfoPlistFile = 'ios/Runner/Info.plist';
 const String yamlKey = 'flutter_launcher_name';
-const String iOSProjectPbxprojFile =
-    'ios/Runner/Runner.xcodeproj/project.pbxproj';
+const String iOSProjectPbxprojFile = 'ios/Runner.xcodeproj/project.pbxproj';
